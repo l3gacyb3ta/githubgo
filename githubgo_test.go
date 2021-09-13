0f617dfd151f18a086b5269011b1b43ec3d2525c
Arcade Wise <l3gacy.b3ta@disroot.org>
2021-09-13 14:28:26 -0400

    feat(tests): add beggings of tests!

--- githubgo_test.go
+++ githubgo_test.go
@@ -1,5 +1,16 @@
-package git
+package githubgo
 
 import (
 	"testing"
 )
+
+func TestGetUser(t *testing.T) {
+	username := "l3gacyb3ta"
+	wantID := 58434499
+
+	user, err := GetUser(username)
+
+	if user.ID != int64(wantID) || err != nil {
+		t.Fatalf(`GetUser("l3gacyb3ta") = %v, %v, want match for %#q, nil`, user, err, wantID)
+	}
+}
